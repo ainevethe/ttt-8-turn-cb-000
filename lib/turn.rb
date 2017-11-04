c536eedcc7537bb8d13c6027e94ab9496e727eab
Crista Cooper <aine3d@gmail.com>
2017-11-04 02:14:49 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,7 +16,7 @@ def turn(board)
   input = gets.chomp.to_i
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, position, "X")
+    move(board, index, "X")
     display_board(board)
   else
     turn(board)
